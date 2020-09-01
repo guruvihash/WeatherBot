bfd6c7585ddf72550f92d11893267309f802305c
GuruVihash <58502568+guruvihash@users.noreply.github.com>
2020-09-01 02:10:17 -0700

    hi

--- TEST.py
+++ TEST.py
@@ -109,7 +109,7 @@ def echo_all(message):
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   bot.process_new_updates([telebot.types.Update.de_json(requests.stream.read().decode("utf-8"))])
    return "!", 200
 @server.route("/")
 def webhook():
